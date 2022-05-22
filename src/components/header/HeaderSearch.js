e154fbe0eb1e0413ad540d39daf63fb0271c434a
Ethan Hawes <ethanhawes@outlook.com>
2022-05-22 18:23:22 +0100

    search bar functionality added

--- src/components/header/HeaderSearch.js
+++ src/components/header/HeaderSearch.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import PageContext from '../store/page-context';
 
 import './HeaderSearch.module.css';
 
 function HeaderSearch() {
-  return <input type="text" placeholder="Search for Recipes" />;
+  const pageCtx = useContext(PageContext);
+
+  function search(event) {
+    event.preventDefault();
+    pageCtx.onChange('Results');
+  }
+
+  return (
+    <form onSubmit={search} action="POST">
+      <input type="text" placeholder="Search for Recipes" />
+    </form>
+  );
 }
 
 export default HeaderSearch;
