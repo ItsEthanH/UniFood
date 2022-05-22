80a15c92ab76d3e69adddeb1b341bb1f9eaa7e59
Ethan Hawes <ethanhawes@outlook.com>
2022-05-23 00:24:14 +0100

    added quantity logic to results sidebar

--- src/components/search/ResultSidebarCard.js
+++ src/components/search/ResultSidebarCard.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import classes from './ResultSidebarCard.module.css';
 
 import placeholder from '../../assets/search/placeholder-sidebar.jpg';
 
 function ResultSidebarCard(props) {
+  const [quantity, setQuantity] = useState(1);
+
+  function increaseQuantity() {
+    setQuantity((qty) => qty + 1);
+  }
+
+  function decreaseQuantity() {
+    setQuantity((qty) => qty - 1);
+  }
+
   return (
     <div className={classes.card}>
       <img src={placeholder} alt="placeholder" />
       <div className={classes.text}>
         <p>Velvet Victoria Cake</p>
-        <button>-</button>
-        <p className={classes.qty}>1</p>
-        <button>+</button>
+        <button onClick={decreaseQuantity}>-</button>
+        <p className={classes.qty}>{quantity}</p>
+        <button onClick={increaseQuantity}>+</button>
       </div>
       <div className={classes.date}>
         <p>Uncatagorised</p>
