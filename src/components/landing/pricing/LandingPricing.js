579bfa6ed3366e6284f688825c3a10eed222f164
Ethan <89361954+ItsEthanH@users.noreply.github.com>
2022-06-14 19:37:47 +0100

    Added pricing paragraph

--- src/components/landing/pricing/LandingPricing.js
+++ src/components/landing/pricing/LandingPricing.js
@@ -8,6 +8,10 @@ function LandingPricing() {
   return (
     <LandingSection id="pricing">
       <LandingTitle>Pricing</LandingTitle>
+      <p className={classes.paragraph}>
+        With three different pricing plans to choose from, there is something
+        for even the fussiest of eaters!
+      </p>
       <div className={classes.cards}>
         <PricingCard styles={classes.bronze} type={0} />
         <PricingCard styles={classes.silver} type={1} />
