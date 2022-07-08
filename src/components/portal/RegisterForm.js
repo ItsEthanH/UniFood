ab1f7609d56be36aae5799837002ee97c19a2f21
Ethan Hawes <ethanhawes@outlook.com>
2022-07-08 23:52:13 +0100

    began setting up register form - broken!

--- src/components/portal/RegisterForm.js
+++ src/components/portal/RegisterForm.js
@@ -1,8 +1,38 @@
+import { useRef } from 'react';
+import useFetch from '../../hooks/useFetch';
+
 import classes from './PortalForm.module.css';
 
-function submitHandler() {}
-
 function RegisterForm() {
+  const { sendRequest, response, isLoading, error } = useFetch();
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const confirmPasswordInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const enteredEmailValue = emailInputRef.current.value;
+    const enteredPasswordValue = passwordInputRef.current.value;
+    const enteredConfirmPasswordValue = confirmPasswordInputRef.current.value;
+
+    const options = {
+      method: 'POST',
+      mode: 'cors',
+      body: JSON.stringify({
+        email: enteredEmailValue,
+        password: enteredPasswordValue,
+        'confirm-password': enteredConfirmPasswordValue,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+    };
+
+    sendRequest('/register', options, 'REGISTER');
+  }
+
   return (
     <form className={classes.form} action="POST" onSubmit={submitHandler}>
       <label for="fname">First Name</label>
@@ -12,13 +42,17 @@ function RegisterForm() {
       <input type="text" name="lname" />
 
       <label for="email">Email</label>
-      <input type="text" name="email" />
+      <input type="text" name="email" ref={emailInputRef} />
 
       <label for="password">Password</label>
-      <input type="password" name="password" />
+      <input type="password" name="password" ref={passwordInputRef} />
 
       <label for="confirm-password">Confirm Password</label>
-      <input type="password" name="confirm-password" />
+      <input
+        type="password"
+        name="confirm-password"
+        ref={confirmPasswordInputRef}
+      />
       <button>Sign Up</button>
     </form>
   );
