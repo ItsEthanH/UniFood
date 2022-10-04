b5a521b6789740f5647d9efa452fc91c73a97bcd
Ethan Hawes <ethanhawes@outlook.com>
2022-10-04 17:50:23 +0100

    rudementary sideways scroll implemented

--- src/pages/book/_BookPage.js
+++ src/pages/book/_BookPage.js
@@ -2,12 +2,36 @@ import BookOptions from './BookOptions';
 import BookCard from './BookCard';
 
 import classes from './styles/BookPage.module.css';
+import { useEffect } from 'react';
+import { useRef } from 'react';
 
 function _BookPage(props) {
+  const scrollSectionRef = useRef();
+
+  useEffect(() => {
+    const el = scrollSectionRef.current;
+
+    function onWheel(event) {
+      event.preventDefault();
+      el.scrollTo({
+        left: el.scrollLeft + event.deltaY * 5,
+        behavior: 'smooth',
+      });
+    }
+
+    el.addEventListener('wheel', onWheel);
+    return () => el.removeEventListener('wheel', onWheel);
+  }, []);
+
   return (
     <main className={classes.main}>
       <BookOptions />
-      <section className={classes.recipes}>
+      <section ref={scrollSectionRef} className={classes.recipes}>
+        <BookCard />
+        <BookCard />
+        <BookCard />
+        <BookCard />
+        <BookCard />
         <BookCard />
         <BookCard />
         <BookCard />
